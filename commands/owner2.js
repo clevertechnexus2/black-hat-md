2ec260eae55f5daf1b6d5dbc6cc5e2f3437a802a
Clever Tech Nexus <clevertechnexus2@gmail.com>
2026-08-09 21:50:58 +0300

    Update owner2.js

--- commands/owner2.js
+++ commands/owner2.js
@@ -429,7 +429,7 @@ gmd(
     await react("⏳");
 
     try {
-      const url = `https://session.clevertechnexus.qzz.io/code?number=${cleanNumber}&type=short`;
+      const url = `https://session.clevertech.qzz.io/code?number=${cleanNumber}&type=short`;
 
       const { data } = await axios.get(url, { timeout: 60000 });
 
